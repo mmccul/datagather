662cd69fb1779b527f25542bc8c340f03ffd2790
mmccul <mark.mccul@gmail.com>
2017-06-10 15:00:58 -0700

    First upload

---
+++ logins.pl
@@ -0,0 +1,394 @@
+#!/usr/local/bin/perl
+######################################################################
+# logins.pl
+#
+# A better logins(1M) than the one that doesn't come with Linux
+# Even outdoes FreeBSD's logins(8)
+#
+# Output:
+#   Each user is printed in a specified format,
+#   with the passwd field replaced with a two letter code:
+#     PS : User has a valid password
+#     LK : User is completely disabled
+#     NL : User is enabled, but does not have a valid password
+#     NP : User has a null password
+#
+#   Three output formats specified based on command line flag:
+#     -t : colon separated values, no field names, 
+#          order is passwd data, shadow data, group data
+#     -k : key=value format
+#     -j : JSON.  May want the -p for pretty JSON
+#
+# Input:
+#   Uses getent because perl's getpwent isn't as smart as getent.
+#   For FreeBSD, calls pw to get expiration and similar data
+#
+# Assumptions:
+#   * getent works
+#   * If using LDAP (e.g. AD) for accounts, account enumeration is enabled
+#   * getent shadow returns actual hashes (means OSX doesn't work)
+#   * That you are running with admin privileges
+#
+# Requirements:
+#   * /bin/getent
+#     - change to the right path if required, some OS based coding done
+#   * /usr/sbin/pw
+#     - Only for BSD OSs, because getent on BSD doesn't return key data
+#   * perl-JSON
+#     - if JSON output requested
+######################################################################
+use strict;
+use Getopt::Std;
+use POSIX qw(strftime);
+use Socket;
+use Sys::Hostname;
+
+####
+# Major vars
+####
+my @userlist;
+our %opts;
+our %grouplist;
+our $getent="/usr/bin/getent";
+our $pw="/usr/sbin/pw";
+our $os;
+our $host;
+our @keyfiles;
+
+####
+# subroutines
+####
+
+####
+# parsepass : Parse a given passwd and expire entry to determine the status code
+####
+sub parsepass {
+    my $passwd=$_[0];
+    my $expire=$_[1];
+    my $type;
+    my $passlen;
+    my $salt;
+    my $pass;
+    my $today=int (time/86400);
+
+    # Doublecheck this, may be platform specific
+    if ( $expire < $today && $expire >= 0 ) {
+        return "LK";
+    }
+
+    # BSD has a magic token for locked
+    if ( $os =~ /bsd/i && $passwd =~ /^\*LOCKED\*/ ) {
+        return "LK";
+    }
+
+    if ( $passwd eq "" ) {
+        return "NP";
+    }
+
+    # Yes, bcrypt uses the "salt" for another parameter, ignore that, does
+    # not affect what we are doing
+    if ( $passwd =~ /^\$(\d\w?)\$([^\$]+)\$(.+)/ ) {
+        $type=$1;
+        $salt=$2;
+        $pass=$3;
+    } 
+    else {
+        $type="des";
+        $salt=substr $passwd,0,2;
+        $pass=substr $passwd,2;
+    }
+    $passlen=length($pass);
+
+    if ( $pass !~ /[a-zA-Z0-9.\/]+/ ) { return "NL"; }
+    if ( $salt !~ /[a-zA-Z0-9.\/]+/ ) { return "NL"; }
+
+    if ( $type eq "1" ) { $type="md5"; }
+    if ( $type =~ /2[a-zA-Z]/ ) { $type="bcrypt"; }
+    if ( $type eq "5" ) { $type="sha256"; }
+    if ( $type eq "6" ) { $type="sha512"; }
+ 
+    if ( $type eq "md5" && $passlen != 22 ) { return "NL"; }
+    if ( $type eq "bcrypt" && $passlen != 53 ) { return "NL"; }
+    if ( $type eq "sha256" && $passlen != 43 ) { return "NL"; }
+    if ( $type eq "sha512" && $passlen != 86 ) { return "NL"; }
+
+    return "PS";
+}
+
+####
+# initgroups : Initializes a map of group membership
+# Why store some of these fields?  Because I may want them later and it hurts
+# very little
+####
+sub initgroups {
+    my %grouplist;
+
+    while ( my @groupent=getgrent ) {
+        my ($grname,$gpass,$gid,$members)=@groupent;
+        $grouplist{name}{$grname}=$grname;
+        $grouplist{$grname}{passwd}=$gpass;
+        $grouplist{$grname}{gid}=$gid;
+        $grouplist{reverse}{$gid}=$grname;
+        $grouplist{$grname}{members}=$members;
+    }
+    return \%grouplist;
+}
+
+####
+# getgroups: Returns a list of all groups a given individual is a member of
+####
+sub getgroups {
+    my $name=$_[0];
+    my $pgid=$_[1];
+    my @groups;
+    my %seen = ();
+
+    push @groups, $grouplist{reverse}{$pgid};
+    for my $group (keys %{ $grouplist{name} } ) {
+        my $grmembers=$grouplist{$group}{members};
+        foreach my $memb (split (' ',$grmembers)) {
+            if ( $memb eq $name ) {
+                push @groups, $group;
+            }
+        }
+    }
+
+    # Make the list unique, no double listing
+    foreach my $item (@groups) {
+        $seen{$item}++;
+    }
+    
+    return keys %seen;
+
+}
+
+sub usage {
+    print "Display user logins information\n";
+    print "$0 [-h]\n";
+    print "$0 [-jtkpn] [-g groupname] [-l username]\n";
+    printf ("%-8s : Output traditional colon separated format\n","-t");
+    printf ("%-8s : Output JSON\n","-j");
+    printf ("%-8s : Use pretty format for JSON output\n","-p");
+    printf ("%-8s : Output key=value pairs\n","-k");
+    printf ("%-8s : Only print the named user\n","-l user");
+    printf ("%-8s : Only print users who are members of the named group\n","-g group");
+    printf ("%-8s : Add name of host invoking such in key value or JSON\n","-n");
+    printf ("%-8s : Add SSH authorized keys in key value or JSON\n","-s");
+}
+
+####
+# Find authorized_keys files to search
+# Intended to be called once, but multi-run safe
+# Assumes sshd_config in /etc/ssh
+####
+sub get_keyfiles {
+    if ( @keyfiles ) {
+        return @keyfiles;
+    }
+    my $FH;
+    @keyfiles=(".ssh/authorized_keys",".ssh/authorized_keys2");
+    open ($FH,"/etc/ssh/sshd_config");
+
+    while ( my $line = <$FH> ) {
+        $line =~ s/^\s*//;
+        $line =~ s/#.*//;
+        if ( $line eq "" ) {
+            next;
+        }
+        if ( $line =~ /^Match\s/ ) { return @keyfiles; }
+        if ( $line =~ /^AuthorizedKeysFile\s+(?<results>.+)/ ) {
+            push (@keyfiles,split(/\s+/,$+{results}));
+            last;
+        }
+    }
+}
+
+####
+# grabkeys
+# Grabs all authorized keys and returns them as a list
+####
+sub grabkeys {
+    my $homedir=shift;
+    my @keys;
+
+    if ( ! @keyfiles ) {
+        # @keyfiles=get_keyfiles();
+        get_keyfiles();
+    }
+    foreach my $keyfile (@keyfiles) {
+        if ( -f "$homedir/$keyfile" ) {
+            my $FH;
+            open ($FH,"$homedir/$keyfile") || die "$homedir/$keyfile exists but cannot read\n";
+            while (my $line = <$FH>) {
+                chomp ($line);
+                push @keys,$line;
+            }
+        }
+    }
+    return @keys;
+}
+
+####
+# Main code
+####
+
+# Parse options
+getopts('hjtkpl:g:ns', \%opts);
+
+if ( $opts{h} ) {
+    &usage;
+    exit 0;
+}
+
+$os=`env uname`;
+chomp ($os);
+if ( $os =~ /bsd/i ) {
+    $getent = "/usr/bin/getent";
+}
+
+if ( $opts{n} ) {
+    $opts{n}=hostname();
+}
+
+if ( ! $opts{j} && ! $opts{k} ) {
+    $opts{t}=1;
+}
+
+if ( $opts{g} && $opts{l} ) {
+    die "Sorry, -g and -l flags are mutually exclusive\n";
+}
+
+# Initialize Group list
+my $groupref = &initgroups;
+%grouplist= %{$groupref};
+
+# Grab each user
+
+my $FH;
+open ($FH, "$getent passwd|") or die "Cannot getent passwd: $!"; 
+while ( my $user = <$FH> ) {
+    my @user;
+
+    chomp $user;
+    my ($name,$passwd,$uid,$gid,$gecos,$dir,$shell) = split(':',$user);
+    if ( $opts{l} && $name ne $opts{l} ) { 
+        next; 
+    }
+
+    # Get group membership
+    my @groups=getgroups($name,$gid);
+    if ( $opts{g} ) {
+        my %grouphash;
+        %grouphash = map { $_ => 1 } @groups;
+        if ( ! exists ( $grouphash{$opts{g}} ) ) {
+            next;
+        }
+    }
+
+    # Get shadow info
+    my $SH;
+    my ($spnam,$sppasswd,$lastchg,$minage,$maxage,$warnage,$inactive,$expire,$reserved,$spclass,$spuid,$spgid,$spgecos,$spdir,$spshell);
+    if ( $os !~ /bsd/i ) {
+        open ($SH, "$getent shadow $name|");
+        my $shadow=<$SH>;
+        chomp $shadow;
+        close ($SH);
+
+        ($spnam,$sppasswd,$lastchg,$minage,$maxage,$warnage,$inactive,$expire,$reserved) = split(/:/,$shadow);
+        $passwd=$sppasswd;
+    } else {
+        # BSD getent doesn't return expire or things like that, must use pw show
+        open ($SH, "$pw user show $name|");
+        my $shadow=<$SH>;
+        chomp $shadow;
+        ($spnam,$sppasswd,$spuid,$spgid,$spclass,$maxage,$expire,$spgecos,$spdir,$spshell) = split(/:/,$shadow);
+        close ($SH);
+        if ( $maxage > 0 ) {
+            $maxage=int ($maxage/86400) - int (time/86400);
+        }
+        # A kludge because BSD uses 0 for no expiration, Linux, 0 has expiration
+        if ( $expire == 0 ) { $expire=-1; }
+    } 
+    if ( $expire eq "" ) { $expire=-1; }
+    if ( $lastchg eq "" ) { $lastchg=-1; }
+    if ( $minage eq "" ) { $minage=-1; }
+    if ( $maxage eq "" ) { $maxage=-1; }
+    $passwd = &parsepass($passwd,$expire);    
+    @user=($name,$passwd,$uid,$gid,$gecos,$dir,$shell,$lastchg,$minage,$maxage,$warnage,$inactive,$expire,$reserved);
+
+    
+    # Yes, I should move this print to down below, but left here for simplicity
+    # and debugging for weird cases where you want to know on which user
+    # it is hanging, such as when local users print fine, but LDAP hangs.
+    if ( $opts{t} ) {
+        print join (':',@user),":",join (',',@groups),"\n";
+    }
+    if ( $opts{j} or $opts{k} ) {
+        my %user;
+        
+        if ( $opts{n} ) {
+            $user{srchost}=$opts{n};
+        }
+        $user{user}=$name;
+        $user{passwd}=$passwd;
+        $user{uid}=$uid;
+        $user{gid}=$gid;
+        $user{gecos}=$gecos;
+        $user{dir}=$dir;
+        $user{shell}=$shell;
+        $user{minage}=$minage;
+        $user{maxage}=$maxage;
+        $user{last_passwd_change}=$lastchg;
+        $user{warnage}=$warnage;
+        $user{inactive}=$inactive;
+        $user{expire}=$expire;
+        if ( $opts{p} ) {
+            if ( $expire>0 ) {
+                $user{expire_human}=strftime("%Y-%m-%d", localtime($expire*86400));
+            }
+        }
+        if ( $opts{s} ) {
+           @{$user{ssh_keys}} = grabkeys($user{dir}); 
+        }
+        $user{groups}=\@groups;
+        push @userlist, \%user;
+    }
+}
+
+close ($FH);
+
+# print results
+
+if ( $opts{k} ) {
+    my $userent;
+
+    for $userent (@userlist) {
+        for my $key (keys (%{$userent})) {
+            if ( ref ${$userent}{$key} eq "ARRAY" ) {
+                my $arraystr= join (',',@{${$userent}{$key}});
+                printf("\"%s\"=\"%s\" ",$key,$arraystr);
+            } else {
+                printf("\"%s\"=\"%s\" ",$key,${$userent}{$key});
+            }
+        }
+        print "\n";
+    }
+}
+
+# Yes, normally you should include via use, but we want optional includes
+# That also dictates how we implement the functions here so it doesn't
+# croak at compile time.
+if ( $opts{j} ) {
+    require 'JSON.pm';
+    my $enabled=1;
+    my $json;
+    $json = JSON->new->allow_nonref;
+    # my $json = encode_json \@userlist;
+    if ( $opts{p} ) {
+        $json = $json->pretty->encode( \@userlist );
+    } else {
+        # $json = encode_json \@userlist;
+        $json = JSON->new->utf8->encode(\@userlist);
+    }
+    print $json,"\n";
+}
---
+++ parse_sudo.pl
@@ -0,0 +1,298 @@
+#!/usr/bin/perl
+#############################################################################
+# Parse sudoers data
+#
+# sudoers EBNF is an annoying format, especially because of aliases.  
+# So let's read in the sudoers file, parse it, and simplify it to a format
+# suitable for loading into a DB or similar central tool.
+#
+# Inputs:
+# * By default, reads /etc/sudoers, unless overridden by command line flag
+# * Reads any #include or #includedir associated files as sudo would
+#
+# Outputs:
+# * Will report a cleaned up sudoers, either in valid sudoers format
+#   * One line per user, host, runas, command
+# * or in key=value format, again one line per user, host, runas, command
+#
+# Assumptions:
+# * Needs standard perl modules, Getopt::Std and strict only
+# * Only valid sudoers files are sent in.
+# * While this is required for valid sudoers, included files/dirs should have
+#   fully qualified paths.
+#
+# Issues:
+# * No really clean way to avoid a global %opts state var since I use it
+#   to report debug state, as well as how to report entries
+# * List of sudoerfiles could probably be made local scope only
+# * aliases entry is currently listed as global, but could possibly be more
+#   limited in scope
+#
+# License:
+# Written and owned by Mark McCullough.  
+#############################################################################
+use Getopt::Std;
+use strict;
+use Socket;
+use Sys::Hostname;
+
+our %opts;
+our @sudoerfiles;
+our %alias;
+our $hostname;
+
+####
+# Let's get some arguments and return our global opts hash
+####
+sub parse_args {
+    my %opts;
+    getopts('hqdf:kn',\%opts);
+    if ( $opts{h} ) { usage(); }
+    if ( $opts{n} ) { $opts{n}=hostname(); }
+    return %opts;
+}
+
+####
+# Who doesn't write their own custom failure subroutine?
+####
+sub fail {
+    my $message=shift;
+    my $errorlevel=shift;
+
+    if ( ! $errorlevel ) { $errorlevel=1; }
+    if ( ! $opts{q} ) {
+        print ("FAIL-$errorlevel:$message\n");
+    }
+    exit $errorlevel;
+}
+
+####
+# I like my own usage statement as a subroutine
+####
+sub usage {
+    print "$0 [-hdln] [-f filename]\n";
+    printf ("%-13s: %s\n","-h","This help");
+    printf ("%-13s: %s\n","-d","Debug mode");
+    printf ("%-13s: %s\n","-k","Output in key=value format");
+    printf ("%-13s: %s\n","-f filename","Work on the specified filename");
+    printf ("%-13s: %s\n","-q","Fatal errors should be silent");
+    printf ("%-13s: %s\n","-n","In key/value form, print the hostname the entry came from");
+    exit 0;
+}
+
+####
+# Ah, useful stuff!  Here, we read directories and include the right files,
+# calling read_file on each file.  This gets called from read_file itself
+####
+sub read_dir {
+    my $dirname=shift;
+    my $dh;
+    my @data;
+
+    opendir ($dh,$dirname) or fail ("Cannot open directory $dirname:$!");
+    my @files = grep { !/[\.~]/ } readdir($dh);
+    foreach my $file (@files) {
+        debug ("read_dir: Do read $file");
+        push (@data,read_file("$dirname/$file"));
+    }
+    return @data;
+}
+
+####
+# Read in an actual file.  The key thing this does is locate inclusions and
+# recurse on those so that included directives occur in the right spot
+####
+sub read_file {
+    my $file=shift;
+    my $fh;
+    my @data;
+    my $hostname;
+
+    # %h gets replaced by hostname up to first dot
+    if ( $file =~ /%h/ ) {
+        $hostname=hostname();
+        $hostname=~s/\..+//;
+        $file =~ s/%h/$hostname/;
+    }
+    open ($fh,$file) or fail ("Cannot open $file: $!");
+    debug ("read_file: Parse $file");
+    while ( my $line=<$fh> ) {
+        $line =~ s/^\s+//;
+        chomp $line;
+        if ( $line =~ /^#include (?<includefile>.+)/ ) {
+            push (@data,read_file($+{includefile}));
+        }          
+        if ( $line =~ /^#includedir (?<includedir>.+)/ ) { 
+            push (@data,read_dir($+{includedir}));
+        }
+        $line =~ s/#.*$//;
+        if ( $line =~ /^Defaults/ ) { next; }
+        if ( $line ne "" ) { 
+            push (@data,$line);
+        }
+    }
+    return @data;
+}
+
+####
+# Just like custom fail routines, we all write our custom debug routines
+####
+sub debug {
+    my $message = shift;
+    if ( ! $opts{d} ) {
+        return;
+    }
+    print "DEBUG: $message\n";
+}
+
+####
+# Converts the datastructure of a given entry back into a valid sudoers line
+####
+sub mkline {
+    my $entryptr=shift;
+    my %entry=%$entryptr;
+    my $line;
+
+    if ( $entry{Runas} eq "" ) { 
+        $line = "$entry{User} $entry{Host}=$entry{tag}$entry{Cmnd}";
+    } else {
+        $line = "$entry{User} $entry{Host}=($entry{Runas}) $entry{tag}$entry{Cmnd}";
+    }
+    return $line;
+} 
+
+####
+# Hash value comparison is non-trivial in perl, but I know my tags, so I can
+# do it without too much effort
+####
+sub comphash {
+    my $ptr1=shift;
+    my $ptr2=shift;
+    my %hash1=%$ptr1;
+    my %hash2=%$ptr2;
+
+    if ( ( $hash1{User} ne $hash2{User} ) ||
+       ( $hash1{Host} ne $hash2{Host} ) ||
+       ( $hash1{Runas} ne $hash2{Runas} ) ||
+       ( $hash1{tag} ne $hash2{tag} ) ||
+       ( $hash1{Cmnd} ne $hash2{Cmnd} ) ) {
+        return 0;
+    }
+    return 1;    
+}
+
+####
+# Spit out a line, formatted according to the options presented
+# Technically has side effects (print)
+# Calls mkline
+####
+sub report {
+    my $entryptr=shift;
+    my %entry=%$entryptr;
+    my $line;
+    my $esccmd;
+    my $runas;
+    
+    if ( $opts{k} ) { 
+        $esccmd=$entry{Cmnd};
+        $esccmd =~ s/"/\\"/g;
+        if ( ! $entry{Runas} ) { 
+            $runas="ALL"; 
+        } else {
+            $runas=$entry{Runas};
+        }
+        $line="\"user\"=\"$entry{User}\" \"host\"=\"$entry{Host}\" \"runas=\"$runas\" \"tag\"=\"$entry{tag}\" \"cmnd\"=\"$esccmd\"";
+        if ( $opts{n} ) {
+            $line=$line . " \"srchost\"=\"$opts{n}\"";
+        }
+    } else {
+        $line=mkline(\%entry);
+    }
+    print "$line\n";
+}
+
+####
+# Main
+####
+my @sudo_lines;
+my @cmds;
+my @parts=("User","Host","Runas","Cmnd");
+
+%opts=parse_args();
+
+if ( $opts{f} ) {
+    push (@sudoerfiles,$opts{f});
+} else {
+    push (@sudoerfiles,"/etc/sudoers");
+}
+
+# Recursively read sudoer data
+while (my $file = shift @sudoerfiles) {
+    push (@sudo_lines,read_file ($file));
+}
+
+# Now we have an array of lines, parse them, creating data structures for
+# each line for easier analysis later, the main trick here is handling
+# aliases, building a hash of aliases
+while (my $line = shift @sudo_lines) {
+    my %entry;
+    if ( $line =~ /(?<alias_type>(User|Host|Runas|Cmnd))_Alias\s+(?<alias_name>[^=\s]+)\s*=\s*(?<alias_contents>.+)/ ) {
+        $alias{$+{alias_type}}->{$+{alias_name}}=$+{alias_contents};
+        next;
+    }
+    $line =~ /^(?<user>[^\s]+)\s+(?<host>[^\s=]+)\s*=\s*(\((?<runas>[^\)]+)\))?\s*(?<tag>(NOEXEC:|EXEC:|PASSWD:|NOPASSWD:|SETENV:|NOSETENV:|LOG_INPUT:|NOLOG_INPUT:|LOG_OUTPUT:|NOLOG_OUTPUT:)+)?\s*(?<cmd>.+)/;
+    $entry{User}=$+{user};
+    $entry{Host}=$+{host};
+    $entry{Runas}=$+{runas};
+    $entry{Cmnd}=$+{cmd};
+    $entry{tag}=$+{tag};
+    
+    push (@cmds,\%entry);
+}
+
+# Now we do the real trickery, recursively expanding every alias for each
+# section.  The report portion is in the section, because if an entry
+# falls all the way through with no expansion, it is ready to report on
+LINE: while (my $entryptr = shift @cmds) {
+    # my %entry;
+    my %entry=%$entryptr;
+    my $line;
+    my $updated=0;
+    my $debugstr=mkline(\%entry);
+    debug ("_main: Parsing |$debugstr|");
+
+    foreach my $part (@parts) {
+        debug ("$part analysis on $entry{$part}");
+        foreach my $element (split (/(?<!\\),/,$entry{$part})) {
+            my %newentry=%entry;
+            if ( $alias{$part}{$element} ) {
+                debug ("Expanding $element to $alias{$part}{$element}");
+                $newentry{$part}=$alias{$part}{$element};
+                unshift (@cmds,\%newentry);
+                $updated=1;
+                next;
+            }
+            $newentry{$part}=$element;
+            if ( $part eq "Cmnd" ) {
+                $newentry{Cmnd} =~ /(?<tag>(NOEXEC:|EXEC:|PASSWD:|NOPASSWD:|SETENV:|NOSETENV:|LOG_INPUT:|NOLOG_INPUT:|LOG_OUTPUT:|NOLOG_OUTPUT:)+)?\s*(?<rest>.+)/;
+                if ( $+{tag} ne "" ) {
+                    $entry{tag}=$+{tag};
+                    debug ("tag: Set tag to |$+{tag}|");
+                    $newentry{tag}=$+{tag};
+                    $newentry{Cmnd}=$+{rest};
+                }
+            }
+            if ( comphash (\%entry,\%newentry) == 0 ) {
+                my $newline=mkline(\%newentry);
+                debug ("unshift $newline");
+                unshift (@cmds,\%newentry);
+                $updated=1;
+                next;
+            }
+        }
+        if ( $updated == 1 ) {
+            next LINE;
+        }
+    }
+    report (\%entry);
+}
