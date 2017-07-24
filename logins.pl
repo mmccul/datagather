940a24660160f7825f760ade61bb74c52ab02a88
mmccul <mark.mccul@gmail.com>
2017-07-24 12:06:40 -0700

    Hey, how did this not get added?

--- logins.pl
+++ logins.pl
@@ -200,8 +200,8 @@ sub get_keyfiles {
             next;
         }
         if ( $line =~ /^Match\s/ ) { return @keyfiles; }
-        if ( $line =~ /^AuthorizedKeysFile\s+(?<results>.+)/ ) {
-            push (@keyfiles,split(/\s+/,$+{results}));
+        if ( $line =~ /^AuthorizedKeysFile\s+(.+)/ ) {
+            push (@keyfiles,split(/\s+/,$1));
             last;
         }
     }
